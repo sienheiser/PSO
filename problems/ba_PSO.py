33536883d6c99c6dc2afeed9df5c5bc097f36c82
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-03-20 11:17:05 +0100

    Code is running
    This is not the final update, the code seems to be reaching to a cost of 0.7, a much better cost exists, will try to tweak the code with more advanced methods.

--- problems/ba_PSO.py
+++ problems/ba_PSO.py
@@ -27,7 +27,7 @@ class Particle:
         self.best_cost_i = -1
         
         for i in noisy_points:
-            self.position_i.append(ma.Vec(np.random.uniform(-2,2,len(i))))
+            self.position_i.append(ma.Vec(np.random.uniform(-5,5,len(i))))
             
         for i in self.position_i:
             self.velocity_i.append(ma.Vec(np.random.uniform(-1,1,len(i))))
@@ -51,7 +51,7 @@ class Swarm:
         
         for particle in self.swarm:
             particle.cost_i = ba(particle.position_i)
-            print('cost_i',particle.cost_i)
+#            print('cost_i',particle.cost_i)
             if particle.best_cost_i>particle.cost_i or particle.best_cost_i == -1:
                 particle.best_cost_i = particle.cost_i
                 particle.best_pos_i = particle.position_i
@@ -65,34 +65,67 @@ class Swarm:
             c1=1        # cognative constant
             c2=2        # social constant
             for particle in self.swarm:
-                for vel_i in particle.velocity_i:
-                    print('this is vel_i',vel_i)
-                    for index in range(len(vel_i)):
-                        r1 = random.random()
-                        r2 = random.random()
-                        vel_cognitive=c1*r1*(particle.best_pos_i[index]-particle.position_i[index])
-                        vel_social=c2*r2*(self.best_pos_g[index]-particle.position_i[index])
-                        vel = w*vel_i[index]+vel_cognitive+vel_social
-                        print('======================',vel)
-                        vel_i[index] = w*vel_i[index]+vel_cognitive+vel_social
-                    print('this is vel_i updated',vel_i)
+                for vel_i in range(len(particle.velocity_i)):
+#                    print('velocity_i',particle.velocity_i[vel_i])
+                    r1 = random.random()
+                    r2 = random.random()
+                    vel_cognitive=c1*r1*(particle.best_pos_i[vel_i]-particle.position_i[vel_i])
+                    vel_social=c2*r2*(self.best_pos_g[vel_i]-particle.position_i[vel_i])
+                    particle.velocity_i[vel_i] = w*particle.velocity_i[vel_i]+vel_cognitive+vel_social
+#                    print('this is velocity_i updated',particle.velocity_i[vel_i])
                     
+    def update_position(self):
+        for particle in self.swarm:
+            for pos_i in range(len(particle.position_i)):
+#                print('pos_i before update',particle.position_i[pos_i])
+                particle.position_i[pos_i] = particle.position_i[pos_i]+particle.velocity_i[pos_i]
+#                
+#                for i in range(len(particle.position_i[pos_i])):
+#                    if particle.position_i[pos_i][i] < bounds[0]:
+#                        particle.position_i[pos_i][i] = bounds[0]
+#                    if particle.position_i[pos_i][i] > bounds[1]:
+#                        particle.position_i[pos_i][i] = bounds[1]
+                    
+#                print('pos_i after update',particle.position_i[pos_i]) 
 
               
             
-            
+#%% creating PSO class which has algorithm
+
+class PSO:
+    def __init__(self,noisy_points,costfunc,num_particles:int,num_iterations:int):
+        self.best_cost = None
+        self.best_position = None
+        
+        sw = Swarm(noisy_points,num_particles)
+        i = 0
+        while i<num_iterations:
+            sw.evaluate(costfunc)
+            sw.update_velocity()
+            sw.update_position()
+            i += 1
+        
+        self.best_cost = sw.best_cost_g
+        self.best_position = sw.best_pos_g
+        print('The best cost is',self.best_cost)
+        print('The best position is',self.best_position)
+        
+                     
+
             
 #%% initializing the problem
 observations = state.observations
+#print(observations)
 cameras = state.cameras
 noisy_points = state.noisy_points
 
 
 #%% testing particles
-par = Particle(noisy_points)
-print('position_i',par.position_i)
-print('velocity_i',par.velocity_i[0])
-print(len(par.velocity_i[0]))
+#
+#par = Particle(noisy_points)
+#print('position_i',par.position_i[0])
+#print('velocity_i',par.velocity_i[0])
+#print(len(par.velocity_i))
 
 
 #%% defining the cost function
@@ -101,15 +134,25 @@ ba = partial(state.bundle_adjust,observations,cameras)
 
 #%% initilizing the swarm
 
-s = Swarm(noisy_points,2)
+#s = Swarm(noisy_points,2)
 
 #%% testing swarm evaluate
-s.evaluate(ba)
-
-print('best_pos_g',s.best_pos_g)
-print('best_cost_g',s.best_cost_g)
+#s.evaluate(ba)
+#
+#print('best_pos_g',s.best_pos_g)
+#print('best_cost_g',s.best_cost_g)
 
 #%% testing swarm velocity_update
-s.update_velocity()
+#print('velocity_i before update',s.swarm[1].velocity_i[0],s.swarm[0].velocity_i[2])
+#s.update_velocity()
+#print('velocity_i before update',s.swarm[1].velocity_i[0],s.swarm[0].velocity_i[2])
 
-#%% testing if __setitem__ works
+#%% testing swarm position_update
+#s.update_position([-0.5,0.5])
+
+#%% testing PSO class
+
+po = PSO(noisy_points,ba,500,20)
+
+
+#%%
