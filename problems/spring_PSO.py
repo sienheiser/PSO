eefd17a0e35d9e3e3159de38d112d524fbc951a4
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-03-20 12:07:26 +0100

    Code for optimizing spring problem
    This is the code for optimizing the spring problem. The optimizer built in ba_PSO is used here.

--- problems/spring_PSO.py
+++ problems/spring_PSO.py
@@ -4,4 +4,46 @@ Created on Fri Mar 20 11:15:29 2020
 
 @author: coolb
 """
+#%%
+import ba_PSO as ba
+#%%
+
+
+
+#%% points of ends of springs
+pts =  [ba.ma.Vec(v) for v in [-2, -1, 0, 0.5, 1.5, 2.5]]
+
+klist = [1,-1,1,-1,1]
+
+#%% residual of springs
+def residual(k, v1, v2):
+    return v2[0] - v1[0] - k
+
+#%% cost function
+
+def costfun(klist,pts):
+    '''
+    klist: list of k values
+    pts: point
+    '''
+    cost = 0
+    for i in range(len(klist)):
+        n = i+1
+        cost += residual(klist[i],pts[i],pts[n])**2
+    return cost
+    
+
+costfun(klist,pts)
+    
+#%% initilizing cost function for springs
+
+costfunction = ba.partial(costfun,klist)
+
+#%% running PSO algorithm
+po = ba.PSO(pts,costfunction,50,100)
+
+
+
+
+
 
