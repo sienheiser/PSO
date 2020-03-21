5f14e2d7d2c81529691ec394f77c79b55cc5f532
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-03-21 11:24:46 +0100

    adjusment
    Some thigns where adjusted to the cost function to make it work.

--- problems/PSO_structure_test.py
+++ problems/PSO_structure_test.py
@@ -111,24 +111,20 @@ class PSO:
         
     
 #%%
-            
-pts = np.random.rand(10,2)
-X = [x for x,y in pts]
-Y = [y for x,y in pts]
 
-def residuals(x,y,a,b):
-    return y-a[0]*x-b[0]
-
-def costfunc(pts,pos):#defining the cost function
-    cost = 0
-    for x,y in pts:
-        cost += residuals(x,y,pos[0],pos[1])*residuals(x,y,pos[0],pos[1])
-    return cost
+#pts = np.random.rand(10,2)
+#X = [x for x,y in pts]
+#Y = [y for x,y in pts]
+#
+#def residuals(x,y,a,b):
+#    return y-a[0]*x-b[0]
+#
+#def costfunc(pts,pos):#defining the cost function
+#    cost = 0
+#    for x,y in pts:
+#        cost += residuals(x,y,pos[0],pos[1])*residuals(x,y,pos[0],pos[1])
+#    return cost
 #%%
-
-
-position = [ma.Vec(2),ma.Vec(2)]#[a,b]
-costfunc(pts,position)
 
 
 f = partial(costfunc,pts)
