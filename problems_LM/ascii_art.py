a3b16adffa8e44e24275ab1216950b56f5cc5b26
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-03-29 14:22:51 +0200

    Adding new folder with LM algorithm

---
+++ problems_LM/ascii_art.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 14:47:07 2020
+
+@author: coolb
+"""
+
+from enum import Enum
+
+class Moods(Enum):
+    Cat       = " (=^@^=) "
+    Excited   = "ヽ(^@^)ノ"
+    Worried   = "(~@~メ) "
+    Crying    = "(╥﹏╥)  "
+    Shrug     = "¯\\_(*@*)_/¯"
+    Impatient = "(ﾟ@ﾟ;≡;ﾟ@ﾟ)"
+    Surprise  = "Σ(ﾟ@ﾟ)  "
+    Facepalm = " (－‸ლ) "
+    GivingUp = " ＿|￣|○ "
+
+ASCII_BABY = (
+    '   ,=""=,\n'
+    '  c , _,{\n'
+    '  /\\  @ )                 __\n'
+    ' /  ^~~^\\          <=.,__/ \'}=\n'
+    '(_/ ,, ,,)          \_ _>_/~\n'
+    ' ~\\_(/-\\)\'-,_,_,_,-\'(_)-(_) )\n')
