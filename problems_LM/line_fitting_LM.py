90fa6c5e880d0a608519b72b90b5c3d64f977377
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-04-04 16:54:07 +0200

    Commented out a script

--- problems_LM/line_fitting_LM.py
+++ problems_LM/line_fitting_LM.py
@@ -35,60 +35,68 @@ def vert_dist(x,y,a,b):
     '''
     return y-a[0]*x-b[0]
 
+pts = opt.np.random.rand(10,2)
+li = [opt.Vec(1),opt.Vec(1)]
+u = opt.Optimizer()
+for x,y in pts:
+    u.add_residual(opt.partial(vert_dist,x,y),li[0],li[1])
+u.optimize()
+print(li)
 
+#%%
 
-iterations = 10000
-i = 0
-avg_time = 0
-avg_iter = 0
-
-lis_time = []
-lis_iter = []
-
-time1 = time.time()
-while i < iterations:
-
-    pts = opt.np.random.rand(10,2)
-    li = [opt.Vec(1),opt.Vec(1)]
-    u = opt.Optimizer()
-    
-    for x,y in pts:
-        u.add_residual(opt.partial(vert_dist,x,y),li[0],li[1])
-        
-    t0 = time.time()
-    
-    u.optimize()
-    
-    t1 = time.time()
-    
-    avg_iter += u.iterations/iterations
-    avg_time += (t1-t0)/iterations
-    
-    lis_time.append(t1-t0)
-    lis_iter.append(u.iterations)
-    
-    i += 1
-time2 = time.time()
-print('The time for the while iterations to finish',time2-time1)
-
-print('average iterations',avg_iter)
-print('average_time',avg_time)
+#iterations = 10000
+#i = 0
+#avg_time = 0
+#avg_iter = 0
+#
+#lis_time = []
+#lis_iter = []
+#
+#time1 = time.time()
+#while i < iterations:
+#
+#    pts = opt.np.random.rand(10,2)
+#    li = [opt.Vec(1),opt.Vec(1)]
+#    u = opt.Optimizer()
+#    
+#    for x,y in pts:
+#        u.add_residual(opt.partial(vert_dist,x,y),li[0],li[1])
+#        
+#    t0 = time.time()
+#    
+#    u.optimize()
+#    
+#    t1 = time.time()
+#    
+#    avg_iter += u.iterations/iterations
+#    avg_time += (t1-t0)/iterations
+#    
+#    lis_time.append(t1-t0)
+#    lis_iter.append(u.iterations)
+#    
+#    i += 1
+#time2 = time.time()
+#print('The time for the while iterations to finish',time2-time1)
+#
+#print('average iterations',avg_iter)
+#print('average_time',avg_time)
 
 #%% calculating standard deviation of time and iterations
-va_time = 0
-va_iter = 0
-
-for ti in lis_time:
-    va_time += (ti-avg_time)**2
-
-for it in lis_iter:
-    va_iter += (it-avg_iter)**2
-    
-sd_time = opt.np.sqrt(va_time/iterations)
-sd_iter = opt.np.sqrt(va_iter/iterations)
-
-print('Standard deviation of time',sd_time)
-print('Standard deviation of iterations',sd_iter)
+#va_time = 0
+#va_iter = 0
+#
+#for ti in lis_time:
+#    va_time += (ti-avg_time)**2
+#
+#for it in lis_iter:
+#    va_iter += (it-avg_iter)**2
+#    
+#sd_time = opt.np.sqrt(va_time/iterations)
+#sd_iter = opt.np.sqrt(va_iter/iterations)
+#
+#print('Standard deviation of time',sd_time)
+#print('Standard deviation of iterations',sd_iter)
 
 
     
