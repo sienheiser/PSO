5e5149d78874fa4424431f8177482606f5a01888
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-04-09 15:26:31 +0200

    Added some print statements

--- problems_PSO/ba_PSO.py
+++ problems_PSO/ba_PSO.py
@@ -19,12 +19,21 @@ observations = state.observations
 cameras = state.cameras
 noisy_points = state.noisy_points
 
+
+
 #%% defining the cost function
 
 ba = partial(state.bundle_adjust,observations,cameras)
 #%% testing PSO class
 
-po = pt.PSO(noisy_points,ba,50,500)
+po = pt.PSO(noisy_points,ba,50,1e-12)
 
 
 #%%
+print(po.best_cost)
+print('Optimized points \n',po.best_position)
+
+points = state.points
+print('Actual points \n',points)
+
+print('Noisy point\n',noisy_points)
