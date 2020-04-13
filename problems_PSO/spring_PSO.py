44aab33378c9419f3f49e33390bbf364bcbf3ed3
sienheiser <31309130+sienheiser@users.noreply.github.com>
2020-04-13 14:49:06 +0200

    Commented

--- problems_PSO/spring_PSO.py
+++ problems_PSO/spring_PSO.py
@@ -12,7 +12,7 @@ import time
 #%% points of ends of springs
 pts =  [pt.ma.Vec(v) for v in [-2, -1, 0, 0.5, 1.5, 2.5]]
 
-klist = [1,-1,1,-1,1]
+klist = [1,1,1,1,1]
 
 #%% residual of springs
 def residual(k, v1, v2):
@@ -44,50 +44,55 @@ po = pt.PSO(pts,costfunction,50,1e-12)
 print('best position',po.best_position)
 print('best cost',po.best_cost)
 
+for i in range(len(po.best_position)-1):
+    n = i+1
+    length = po.best_position[n]-po.best_position[i]
+    print(length)
+
 
 #%% Script for getting average time and iterations
 
-i = 0
-iterations = 10000
-avg_iter = 0
-avg_cost = 0
-avg_time = 0
-
-lis_iter = []
-lis_time = []
-
-while i<iterations:
-    t0 = time.time()
-    po = pt.PSO(pts,costfunction,20,1e-12)
-    t1 = time.time()
-    
-    avg_iter += po.iteration/iterations
-    avg_time += (t1-t0)/iterations
-    
-    lis_iter.append(po.iteration)
-    lis_time.append(t1-t0)
-    
-    i += 1
-    
-    
-print('average iterations',avg_iter)
-print('avg_time',avg_time)
+#i = 0
+#iterations = 10000
+#avg_iter = 0
+#avg_cost = 0
+#avg_time = 0
+#
+#lis_iter = []
+#lis_time = []
+#
+#while i<iterations:
+#    t0 = time.time()
+#    po = pt.PSO(pts,costfunction,20,1e-12)
+#    t1 = time.time()
+#    
+#    avg_iter += po.iteration/iterations
+#    avg_time += (t1-t0)/iterations
+#    
+#    lis_iter.append(po.iteration)
+#    lis_time.append(t1-t0)
+#    
+#    i += 1
+#    
+#    
+#print('average iterations',avg_iter)
+#print('avg_time',avg_time)
 
 #%% calculating standard deviation of time and iterations
-va_time = 0
-va_iter = 0
-
-for ti in lis_time:
-    va_time += (ti-avg_time)**2
-
-for it in lis_iter:
-    va_iter += (it-avg_iter)**2
-    
-sd_time = pt.np.sqrt(va_time/iterations)
-sd_iter = pt.np.sqrt(va_iter/iterations)
-
-print('Standard deviation of time',avg_time,sd_time)
-print('Standard deviation of iterations',avg_iter,sd_iter)
+#va_time = 0
+#va_iter = 0
+#
+#for ti in lis_time:
+#    va_time += (ti-avg_time)**2
+#
+#for it in lis_iter:
+#    va_iter += (it-avg_iter)**2
+#    
+#sd_time = pt.np.sqrt(va_time/iterations)
+#sd_iter = pt.np.sqrt(va_iter/iterations)
+#
+#print('Standard deviation of time',avg_time,sd_time)
+#print('Standard deviation of iterations',avg_iter,sd_iter)
 
 
 
